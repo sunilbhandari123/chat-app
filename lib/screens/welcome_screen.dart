c64c93020cf1bc4607996a354b77ddec6b34a643
Sunil Bhandari <buddhaparsad123@gmail.com>
2022-08-14 22:00:05 +0545

    Animation

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -8,11 +8,50 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  // Animation
+
+  late AnimationController controller;
+  late Animation animation;
+
+  get status => null;
+  @override
+  void initState() {
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+
+    //animation = CurvedAnimation(parent: controller, curve: Curves.decelerate);
+    animation =
+        ColorTween(begin: Colors.black, end: Colors.white).animate(controller);
+
+    controller.forward();
+
+    //animation.addStatusListener((status) {
+    //if (status == AnimationStatus.completed) {
+    //controller.reverse(from: 1.0);
+    //} else if (status == AnimationStatus.dismissed) {
+    //controller.forward();
+    //}
+    //});
+
+    controller.addListener(() {
+      setState(() {});
+      print(animation.value);
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -25,7 +64,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: 60.0,
+                    height:60,
                   ),
                 ),
                 const Text(
